f74436cada51c7707cb8b6f06a06d00309715ae5
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 19:20:13 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region  = "us-east-1"
 
 }
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
 
 resource "aws_instance" "chintu" {
   ami           = "ami-0be2609ba883822ec"
