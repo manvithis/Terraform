f3f02fd0f38ac3b8f0ffa724efb7834fd0ea6daf
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 18:25:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  region  = "us-west-1"
+  region  = "us-east-1"
+
 }
-data "aws_vpc" "default" {
-  default = true
-}
+
 resource "aws_instance" "chintu" {
   ami           = "ami-03ba3948f6c37a4b0"
   instance_type = "t2.micro"
