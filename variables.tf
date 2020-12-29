6f204f3d17a4045d5c800906ad521fdcacb9a275
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 17:26:07 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "ami" {
-  description = "The type of ami to start"
-  type        = string
-  default     = data.aws_ami.ubuntu.id
-}
+# variable "ami" {
+#   description = "The type of ami to start"
+#   type        = string
+#   default     = data.aws_ami.ubuntu.id
+# }
 variable "instance" {
   description = "The type of instance to start"
   type        = string
