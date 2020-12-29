2e2bef1b0e42de553dba23f94e504586a1bd64d4
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 19:50:51 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,9 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0be2609ba883822ec"
-  instance_type = "t3a.small"
-  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
+  instance_type = "t2.small"
   tags = {
     Name = "HelloWorld"
   }
