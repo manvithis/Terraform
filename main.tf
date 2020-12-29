41707ae951e4c50eb48fef7951979aadb2e894d0
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 19:53:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0be2609ba883822ec"
   instance_type = "t2.small"
+  subnet_id = "subnet-027af735e571de719"
   tags = {
     Name = "HelloWorld"
   }
