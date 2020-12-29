490d28dbd29d14377e56548d2211760669a67368
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 19:23:53 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "eu-east-1"
 }
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
