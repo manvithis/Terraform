b2f57c66ad52fbc80af68ac34357ef72e740edf3
Sai Manvith <saimanvith99@gmail.com>
2020-12-28 20:07:27 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,16 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+data "aws_subnet_ids" "private" {
+  vpc_id = "vpc-0c1ca6a9599bb1b14"
+ }
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0be2609ba883822ec"
   instance_type = "t2.small"
-  subnet_id = "subnet-027af735e571de719"
+  
+  subnet_id = "subnet-064a08b59a92c98c5"
   tags = {
     Name = "HelloWorld"
   }
